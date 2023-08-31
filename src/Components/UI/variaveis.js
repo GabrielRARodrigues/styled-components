5ecfd65613012da710678b90d42e69128a1a04af
Gabriel Rodrigues <gabriel.rocha.andrade.rodrigues@gmail.com>
2023-08-31 12:34:28 -0300

    Completing the application, creating the feature for theme switching
    Concluding the application, creating the 'temas.js' file, and using the ThemeProvider component from StyledComponents to implement the functionality of switching between light and dark themes. This involves modifying the 'variaveis.js' file by adding the style variables for each of the themes. Additionally, creating the styled components for 'IconeTema' and 'BtnTema' to modify the icons when toggling between themes, and designing a button for directly changing the themes. Finally, developing the 'SwitcherTema' component to dynamically adjust the icon tones, and utilizing the useState hook to store the state of the currently rendered theme

--- src/Components/UI/variaveis.js
+++ src/Components/UI/variaveis.js
@@ -1,3 +1,12 @@
+//General
 export const corPrimaria = "#41d3be";
+
+// Light mode
 export const fundoClaro = "#f1f1f1";
+export const conteudoClaro = "white";
 export const textoFundoClaro = "grey";
+
+//Dark mode
+export const fundoEscuro = "#363537";
+export const conteudoEscuro = "#5c5b5e";
+export const textoFundoEscuro = "#fafafa";
